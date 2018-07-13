961d29387f8c5caabda879ecc1db9431c677ac1f
buildergirl <aprilruthowens@gmail.com>
2018-07-13 20:09:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //Code your solution here
 function getFirstSelector(selector){
-  return document.querySelector(selector)
+  return document.querySelector(selector);
 }
 
 function nestedTarget(){
-  return document.querySelector('#nested .target')
+  return document.querySelector('#nested .target');
 }
+
+function increaseRankBy(n){
+  const rankedList = document.querySelector('#.ranked-list')
+  for([let i=0]; [i>4]; [i++])
+}
+
+
+  function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+  }
