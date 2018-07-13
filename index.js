3a075023edd71808c1eae35e242016db89a4f866
buildergirl <aprilruthowens@gmail.com>
2018-07-13 17:20:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //Code your solution here
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
